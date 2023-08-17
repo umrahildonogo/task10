734f479a8b218cd7c32d2b354ecad8dc32969b10
raccoon <donogo.umrahil@yandex.ru>
2023-08-18 02:38:03 +0300

    new commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+
+Console.WriteLine ("Введите трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int secondDigit = seconddigit (number);
+Console.WriteLine(secondDigit);
+
+int seconddigit (int num)
+{
+    int firstdigit = num / 100;
+    int seconddigit = num / 10;
+    int result = (num - num / 100 * 100 - num % 10) / 10; //я не знаю, как оно сработало, но оно сработало
+    return result;
+}
